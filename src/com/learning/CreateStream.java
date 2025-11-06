20cf25b1d3c7a91fa3eaf6e415bea21dfd007aa1
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-06 11:39:06 +0530

    - Different ways of using streams in Java.

--- src/com/learning/CreateStream.java
+++ src/com/learning/CreateStream.java
@@ -1,8 +1,27 @@
 package com.learning;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class CreateStream {
 
     public static void main(String[] args) {
-        
+
+        // To convert a list into a stream.
+        List<String> names = Arrays.asList("Tony", "Steve", "Bruce");
+        Stream<String> stream = names.stream();
+
+        // To convert an array into a stream.
+        String[] arr = {"Tony", "Steve", "Bruce"};
+        Stream<String> stream1 = Arrays.stream(arr);
+
+        // Using Stream of
+        Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5);
+
+        // Using Stream generate
+        Stream<String> limit = Stream.generate(() -> "Hello").limit(5);
+
+
     }
 }
