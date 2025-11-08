2ce185283f9043de5bff79a8c15a9f912b6a5bcd
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-08 15:35:00 +0530

    - Practised Intermediate Part - 2 stream questions.  1. MostFrequentCharacter.

---
+++ src/com/learning/IntermediatePart2/MostFrequentCharacter.java
@@ -0,0 +1,44 @@
+package com.learning.IntermediatePart2;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class MostFrequentCharacter {
+
+    public static void main(String[] args) {
+
+        String string = "Banana";
+
+        Map<Character, Long> ans = string
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        System.out.println(ans);
+
+        Optional<Map.Entry<Character, Long>> max = string
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue());
+
+        System.out.println(max);
+
+        Optional<Character> character = string
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey);
+
+        System.out.println(character);
+
+
+    }
+}
