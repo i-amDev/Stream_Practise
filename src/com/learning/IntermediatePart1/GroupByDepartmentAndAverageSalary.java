26d15e1c6bf5b33234707b4c086ccca731616357
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-08 14:19:34 +0530

    - Practised Intermediate Part - 1 stream questions.  1. GroupByDepartmentAndAverageSalary.

---
+++ src/com/learning/IntermediatePart1/GroupByDepartmentAndAverageSalary.java
@@ -0,0 +1,27 @@
+package com.learning.IntermediatePart1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class GroupByDepartmentAndAverageSalary {
+
+    public static void main(String[] args) {
+
+        List<Employee> employees = Arrays.asList(
+                new Employee(101, "Tony", 50000, 60, "HR"),
+                new Employee(102, "Steve", 35000, 80, "IT"),
+                new Employee(103, "Bruce", 80400, 55, "Finance"),
+                new Employee(104, "Peter", 125000, 25, "HR"),
+                new Employee(105, "Thor", 23000, 235, "Finance")
+        );
+
+        Map<String, Double> output = employees
+                .stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment, Collectors.averagingDouble(Employee::getSalary)));
+
+        System.out.println(output);
+
+    }
+}
