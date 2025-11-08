a2168f4ee3518e1579a09407b4cede766339a919
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-08 14:19:18 +0530

    - Practised Intermediate Part - 1 stream questions.  1. CountOccurrences

---
+++ src/com/learning/IntermediatePart1/CountOccurrences.java
@@ -0,0 +1,21 @@
+package com.learning.IntermediatePart1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CountOccurrences {
+
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("bat", "ball", "stumps", "ball", "stumps", "ball");
+
+        Map<String, Long> output = list
+                .stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        System.out.println(output);
+    }
+}
