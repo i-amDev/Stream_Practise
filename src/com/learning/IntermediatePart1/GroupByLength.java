0ebbaaa5a0d012660eaa9048f1ec069fcabf6fff
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-08 14:19:02 +0530

    - Practised Intermediate Part - 1 stream questions.  1. GroupByLengthOfString

---
+++ src/com/learning/IntermediatePart1/GroupByLength.java
@@ -0,0 +1,22 @@
+package com.learning.IntermediatePart1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class GroupByLength {
+
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("Tony", "Steve", "Bruce", "Peter", "May", "Bob");
+
+        Map<Integer, List<String>> output = list
+                .stream()
+                .collect(Collectors.groupingBy(String::length));
+
+        System.out.println(output);
+
+    }
+
+}
