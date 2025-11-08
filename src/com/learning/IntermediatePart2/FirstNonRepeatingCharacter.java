09205b9d77c3c464a14fb54f65b475db7dfb83e5
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-08 15:35:17 +0530

    - Practised Intermediate Part - 2 stream questions.  1. FirstNonRepeatingCharacter.

---
+++ src/com/learning/IntermediatePart2/FirstNonRepeatingCharacter.java
@@ -0,0 +1,34 @@
+package com.learning.IntermediatePart2;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FirstNonRepeatingCharacter {
+
+    public static void main(String[] args) {
+
+        String string = "swiss";
+
+        Map<Character, Long> frequency = string
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        System.out.println(frequency);
+
+        Character character = string
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() == 1)
+                .findFirst()
+                .map(Map.Entry::getKey)
+                .orElseThrow();
+
+        System.out.println(character);
+    }
+}
