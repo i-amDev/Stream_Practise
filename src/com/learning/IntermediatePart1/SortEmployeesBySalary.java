15af22fb3f2bc6db34437aecb1c315ab9bc84bd5
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-12-30 17:00:01 +0530

    - Refactored.

--- src/com/learning/IntermediatePart1/SortEmployeesBySalary.java
+++ src/com/learning/IntermediatePart1/SortEmployeesBySalary.java
@@ -28,5 +28,6 @@ public class SortEmployeesBySalary {
 
         System.out.println("Employees sorted in ascending according to their salary : " + ascendingOrder);
         System.out.println("Employees sorted in descending according to their salary : " + descendingOrder);
+
     }
 }
