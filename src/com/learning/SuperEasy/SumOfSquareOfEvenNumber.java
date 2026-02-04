61cb122c5a1707000118c7665ac17e98cffffb1e
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-02-04 22:08:54 +0530

    - Refactored.

--- src/com/learning/SuperEasy/SumOfSquareOfEvenNumber.java
+++ src/com/learning/SuperEasy/SumOfSquareOfEvenNumber.java
@@ -31,6 +31,5 @@ public class SumOfSquareOfEvenNumber {
         System.out.println("Sum of square of even numbers is : " + result);
         System.out.println("Sum of square of even numbers is : " + ans);
         System.out.println("Sum of square of even numbers is : " + sum.get());
-
     }
 }
